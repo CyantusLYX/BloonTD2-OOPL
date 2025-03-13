7c9f50bae80fd4002528a00c6ae99aee8c37bc39
cyantus <amdyes0201@gmail.com>
2025-03-13 10:10:51 +0800

    Refactors Bloons class and related interfaces
    Updates class and interface names to be more descriptive and accurate
    Renames Bloons class to Bloon for clarity
    Introduces ICollapsible interface for collision detection
    
    Relates to #123

--- include/interfaces.hpp
+++ include/interfaces.hpp
@@ -1,10 +1,19 @@
 #ifndef INTERFACES_HPP
 #define INTERFACES_HPP
+#include <Util/Input.hpp>
 namespace Interface {
 class IUpdatable {
 public:
     virtual void update() = 0;
     virtual ~IUpdatable() = default;
 };
+class ICollapsible {
+public:
+    virtual bool isCollide(const ICollapsible &other) const = 0;
+    virtual ~ICollapsible() = default;
+private:
+    Util::PTSDPosition m_Position;
+};
 }
+
 #endif // INTERFACES_HPP
--- src/bloon.cpp
+++ src/bloon.cpp
@@ -1,6 +1,6 @@
-#include "bloons.hpp"
+#include "bloon.hpp"
 #include "Util/Time.hpp"
-Bloons::Bloons(Type type) : m_Type(type) {
+Bloon::Bloon(Type type) : m_Type(type) {
   switch (type) {
   case Type::red:
     m_SpeedMult = 1.0f;
@@ -9,51 +9,51 @@ Bloons::Bloons(Type type) : m_Type(type) {
   case Type::blue:
     m_SpeedMult = 1.4f;
     m_RBE = 2;
-    m_ChildBloons.push_back(std::make_shared<Bloons>(Type::red));
+    m_ChildBloons.push_back(std::make_shared<Bloon>(Type::red));
     break;
   case Type::green:
     m_SpeedMult = 1.8f;
     m_RBE = 3;
-    m_ChildBloons.push_back(std::make_shared<Bloons>(Type::blue));
+    m_ChildBloons.push_back(std::make_shared<Bloon>(Type::blue));
     break;
   case Type::yellow:
     m_SpeedMult = 3.2f;
     m_RBE = 4;
-    m_ChildBloons.push_back(std::make_shared<Bloons>(Type::green));
+    m_ChildBloons.push_back(std::make_shared<Bloon>(Type::green));
     break;
   case Type::black:
     m_SpeedMult = 1.8f;
     m_RBE = 9;
     for (int _=0; _ < 2; _++)
-      m_ChildBloons.push_back(std::make_shared<Bloons>(Type::yellow));
+      m_ChildBloons.push_back(std::make_shared<Bloon>(Type::yellow));
     break;
   case Type::white:
     m_SpeedMult = 2.5f;
     m_RBE = 9;
     for (int _=0; _ < 2; _++)
-      m_ChildBloons.push_back(std::make_shared<Bloons>(Type::yellow));
+      m_ChildBloons.push_back(std::make_shared<Bloon>(Type::yellow));
     break;
   case Type::lead:
     m_SpeedMult = 1.0f;
     m_RBE = 19;
     for (int _=0; _ < 2; _++)
-      m_ChildBloons.push_back(std::make_shared<Bloons>(Type::yellow));
+      m_ChildBloons.push_back(std::make_shared<Bloon>(Type::yellow));
     break;
   case Type::rainbow:
     m_SpeedMult = 2.2f;
     m_RBE = 37;
     for (int _ = 0; _ < 2; _++) {
-      m_ChildBloons.push_back(std::make_shared<Bloons>(Type::white));
-      m_ChildBloons.push_back(std::make_shared<Bloons>(Type::black));
+      m_ChildBloons.push_back(std::make_shared<Bloon>(Type::white));
+      m_ChildBloons.push_back(std::make_shared<Bloon>(Type::black));
     }
     break;
   }
 }
-void Bloons::setFrozed(float time) {
+void Bloon::setFrozed(float froze_time) {
   m_State = State::frozed;
-  meltTime = Util::Time::GetElapsedTimeMs() + time;
+  meltTime = Util::Time::GetElapsedTimeMs() + froze_time;
 }
-void Bloons::update() {
+void Bloon::update() {
   if (m_State == State::frozed) {
     if (meltTime < Util::Time::GetElapsedTimeMs()) {
       m_State = State::alive;
