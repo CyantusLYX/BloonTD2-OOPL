3915c0c7a7192cf068dae4a381946264c30e15c1
K_ARCH <t112590028@ntut.org.tw>
2025-03-14 23:51:41 +0800

    pseudo-collapsible-solution

--- src/collapsible.cpp
+++ src/collapsible.cpp
@@ -1,9 +1,19 @@
 #include "collapsible.hpp"
 
+//rectangle to rectangle
 bool Collapsible::isCollide(const Collapsible &other) const{
+    if(other.m_Pivot == m_Pivot){//shoud fix the c_box
+        return true;
+    }
     return false;
 }
 
+//rectangle to dot
+//if it trigger every object's isCollide when mouse click
+//it will be so odd, doesn't it?
 bool Collapsible::isCollide(const glm::vec2 pt) const{
+    if(pt == m_Pivot){
+        return true;
+    }
     return false;
 }
