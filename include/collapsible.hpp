3d85685aea4ed223d1104603620534821a33cafa
K_ARCH <t112590028@ntut.org.tw>
2025-03-15 17:30:19 +0800

    collide type to dot collision detect was written but window was no show.

--- include/collapsible.hpp
+++ include/collapsible.hpp
@@ -3,15 +3,31 @@
 #include "Util/GameObject.hpp"
 #include <glm/fwd.hpp>
 
+enum class ColType {DOT, OVAL, RECTANGLE};
 class Collapsible : public Util::GameObject {
 public:
   Collapsible(const std::shared_ptr<Core::Drawable> &drawable,
               const float zIndex, const glm::vec2 &pivot = {0, 0},
+              const glm::vec2 &pcol = {0, 0},
+              const ColType tcol = ColType::RECTANGLE,
               const bool visible = true,
               const std::vector<std::shared_ptr<GameObject>> &children =
-                  std::vector<std::shared_ptr<GameObject>>())
-      : Util::GameObject(drawable, zIndex, pivot, visible, children) {}
-    Collapsible() = default;
+                    std::vector<std::shared_ptr<GameObject>>()
+                    )
+      : Util::GameObject(drawable, zIndex, pivot, visible, children), 
+        m_pcol(pcol), m_tcol(tcol){}
+  Collapsible() = default;
+  
+  /**
+  * @param m_pcol The parameter of collide box.
+  *               If m_tcol == DOT        then m_pcol should be pivot or just {0, 0}
+  *               If m_tcol == OVAL       then m_pcol refers to {w, h} in (x/w)^2 + (y/h)^2 = 1
+  *               If m_tcol == RECTANGLE  then m_pcol refers to {w, h} in x=(+-w/2), y=(+-h/2)
+  * @param m_tcol The type of collide box        
+  */
+  glm::vec2 m_pcol;
+  ColType   m_tcol;
+
   bool isCollide(const Collapsible &other) const;
   bool isCollide(const glm::vec2 pt) const;
   virtual ~Collapsible() = default;
--- src/collapsible.cpp
+++ src/collapsible.cpp
@@ -1,19 +1,75 @@
 #include "collapsible.hpp"
+#include <X11/Xlibint.h>
 
-//rectangle to rectangle
+//ColType to ColType
+//wait to be implement
 bool Collapsible::isCollide(const Collapsible &other) const{
-    if(other.m_Pivot == m_Pivot){//shoud fix the c_box
-        return true;
-    }
     return false;
+
+    switch (m_tcol) {
+        case ColType::DOT:
+            switch (other.m_tcol) {
+                case ColType::DOT:
+                    return false;
+                case ColType::OVAL:
+                    return false;
+                case ColType::RECTANGLE:
+                    return false;
+                default:
+                    throw std::invalid_argument( "err on collapsible::isCollide switch" );
+                    return false;
+            }
+            return false;
+        case ColType::OVAL:
+            switch (Collapsible::m_tcol) {
+                case ColType::DOT:
+                    return false;
+                case ColType::OVAL:
+                    return false;
+                case ColType::RECTANGLE:
+                    return false;
+                default:
+                    throw std::invalid_argument( "err on collapsible::isCollide switch" );
+                    return false;
+            }
+            return false;
+        case ColType::RECTANGLE:
+            switch (Collapsible::m_tcol) {
+                case ColType::DOT:
+                    return false;
+                case ColType::OVAL:
+                    return false;
+                case ColType::RECTANGLE:
+                    return false;
+                default:
+                    throw std::invalid_argument( "err on collapsible::isCollide switch" );
+                    return false;
+            }
+            return false;
+        default:
+            throw std::invalid_argument( "err on collapsible::isCollide switch" );
+            return false;
+    }
 }
 
-//rectangle to dot
+//ColType to dot
 //if it trigger every object's isCollide when mouse click
 //it will be so odd, doesn't it?
 bool Collapsible::isCollide(const glm::vec2 pt) const{
-    if(pt == m_Pivot){
-        return true;
-    }
     return false;
+    switch (m_tcol) {
+        case ColType::DOT:
+            return pt == m_Pivot;
+        case ColType::OVAL:
+            return pow((pt.x-m_Pivot.x)/m_pcol.x, 2) + pow((pt.y-m_Pivot.y)/m_pcol.y, 2) > 1.0;
+        case ColType::RECTANGLE:
+            return  (pt.x < m_Pivot.x + m_pcol.x/2)
+                  &&(pt.x > m_Pivot.x - m_pcol.x/2)
+                  &&(pt.y < m_Pivot.y + m_pcol.y/2)
+                  &&(pt.y > m_Pivot.y - m_pcol.y/2);
+        default:
+            throw std::invalid_argument( "err on collapsible::isCollide switch" );
+            return false;
+    }
+    
 }
