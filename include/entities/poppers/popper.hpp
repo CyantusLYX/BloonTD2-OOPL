2a6dd0138e14de9e85cf4ed86e390588932a9933
cyantus <amdyes0201@gmail.com>
2025-05-05 16:59:14 +0800

    It should not break anything.

--- include/entities/poppers/popper.hpp
+++ include/entities/poppers/popper.hpp
@@ -1,6 +1,7 @@
 #ifndef POPPERS_POPPER_HPP
 #define POPPERS_POPPER_HPP
 #include "Util/GameObject.hpp"
+#include "Util/Image.hpp"
 #include "Util/Input.hpp"
 #include "components/collisionComp.hpp"
 #include "entities/bloon.hpp"
@@ -12,6 +13,7 @@
 class popper : public Mortal, public Components::CollisionComponent {
 protected:
   bool explosive = false;
+
 public:
   popper(const Util::PTSDPosition &pos = {0, 0}, const std::variant<glm::vec2, float> &circle_r_or_rectangle_xy = 0.0f)
       : Components::CollisionComponent(pos, circle_r_or_rectangle_xy) {
@@ -20,5 +22,6 @@ public:
   virtual std::vector<bool> hit(std::vector<std::shared_ptr<Bloon>> bloons) = 0;
   virtual std::shared_ptr<Util::GameObject> get_object(){return nullptr;};
   virtual Util::PTSDPosition get_position() const=0;
+  
 };
 #endif
